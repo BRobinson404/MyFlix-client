7069b0c09f206d34e8955f6c9d71eed5eb73601d
BRobinson404 <brobinso123@gmail.com>
2023-05-27 00:53:14 -0500

    Created MovieCard Component

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,12 @@
+/* This component can be used to render a list of movie cards and handle user interactions with them.*/
+export const MovieCard = ({ movie, onMovieClick }) => {
+    return (
+      <div
+        onClick={() => {
+          onMovieClick(movie);
+        }}
+      >
+        {movie.Title}
+      </div>
+    );
+  };
