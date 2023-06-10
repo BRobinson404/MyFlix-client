33876777e34b1817ff953b3b3548fd087a040de2
BRobinson404 <brobinso123@gmail.com>
2023-06-10 03:32:22 -0500

    update to signup view
    condensed unnecessary code

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -36,73 +36,73 @@ export const SignupView = () => {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit}>
+          <Row>
+            <Col>
+              <Form.Group controlId='formUsername'>
+              <Form.Label>Username:</Form.Label>
+              <Form.Control
+              type='text'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              minLength='3'
+              />
+              </Form.Group>
+            </Col>
+        </Row>
+          <Row>
+            <Col>
+              <Form.Group controlId='formPassword'>
+                <Form.Label>Password:</Form.Label>
+                <Form.Control
+                  type='password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+
       <Row>
         <Col>
-          <Form.Group controlId='formUsername'>
-          <Form.Label>Username:</Form.Label>
-          <Form.Control
-          type='text'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          minLength='3'
-          />
-          </Form.Group>
-        </Col>
-     </Row>
-      <Row>
-        <Col>
-          <Form.Group controlId='formPassword'>
-            <Form.Label>Password:</Form.Label>
+          <Form.Group controlId='formEmail'>
+            <Form.Label>Email:</Form.Label>
             <Form.Control
-              type='password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </Form.Group>
         </Col>
       </Row>
 
-  <Row>
-    <Col>
-      <Form.Group controlId='formEmail'>
-        <Form.Label>Email:</Form.Label>
-        <Form.Control
-          type='email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </Form.Group>
-    </Col>
-  </Row>
+      <Row>
+        <Col>
+          <Form.Group controlId='formBirthday'>
+            <Form.Label>Birthday:</Form.Label>
+            <Form.Control
+              type='date'
+              value={birthday}
+              onChange={(e) => setBirthday(e.target.value)}
+              required
+            />
+          </Form.Group>
+        </Col>
+      </Row>
 
-  <Row>
-    <Col>
-      <Form.Group controlId='formBirthday'>
-        <Form.Label>Birthday:</Form.Label>
-        <Form.Control
-          type='date'
-          value={birthday}
-          onChange={(e) => setBirthday(e.target.value)}
-          required
-        />
-      </Form.Group>
-    </Col>
-  </Row>
-
-  <Row>
-    <Col>
-      <Button type='submit'>Submit</Button>
-    </Col>
-    <Col xs={3}>
-      <Link to={`/login`}>
-          I already have an account.
-      </Link>
-    </Col>
-  </Row>
-</Form>
-);
+      <Row>
+        <Col>
+          <Button type='submit'>Submit</Button>
+        </Col>
+        <Col xs={3}>
+          <Link to={`/login`}>
+              I already have an account.
+          </Link>
+        </Col>
+      </Row>
+    </Form>
+  );
 };
