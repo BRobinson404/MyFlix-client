559a3328e9acba87309c7128325314b2fe53c707
BRobinson404 <brobinso123@gmail.com>
2023-10-14 21:29:51 -0500

    updating movie-view layout

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -4,7 +4,6 @@ import { Row, Col, Button } from 'react-bootstrap';
 
 export const MovieView = ({ movies, user, onAddFavorite }) => {
   const { id } = useParams();
-
   const movie = movies.length ? movies.find((movie) => movie.id === id) : null;
 
   const handleAddFavorite = () => {
@@ -12,64 +11,55 @@ export const MovieView = ({ movies, user, onAddFavorite }) => {
   };
 
   useEffect(() => {
-    console.log("User prop changed:", user);
+    console.log('User prop changed:', user);
   }, [user]);
 
   return (
     <div className="MovieView">
       <Row>
-        {/* Display the movie's image using the imagePath */}
-        <Col>
+        <Col md={4}>
           <img src={movie.ImagePath} className="img-fluid" alt="Movie Poster" />
         </Col>
-      </Row>
 
-      <Row className="row-title">
-        {/* Display the movie's title */}
-        <Col>
-          <span>Title: </span>
-          <span>{movie.Title}</span>
+        <Col md={8}>
+          <Row className="row-title">
+            <Col>
+              <h2>{movie.Title}</h2>
+            </Col>
+          </Row>
+
+          <Row className="row-title">
+            <Col>
+              <span>Description: {movie.Description}</span>
+            </Col>
+          </Row>
+
+          <Row className="row-title">
+            <Col>
+              <span>Genre: {movie.Genre.Name}</span>
+            </Col>
+          </Row>
+
+          <Row className="row-title">
+            <Col>
+              <span>Director: {movie.Director.Name}</span>
+            </Col>
+          </Row>
+
+          <Row className="button-row">
+            <Col>
+              <Link to="/movies" className="btn btn-primary">
+                Back
+              </Link>
+              {user && (
+                <Button className="favorite-button" variant="primary" onClick={handleAddFavorite}>
+                  Favorite &#x2665;
+                </Button>
+              )}
+            </Col>
+          </Row>
         </Col>
       </Row>
-
-      <Row className="row-title">
-        {/* Display the movie's description */}
-        <Col>
-          <span>Description: </span>
-          <span>{movie.Descriptions}</span>
-        </Col>
-      </Row>
-
-      <Row className="row-title">
-        {/* Display the movie's genre */}
-        <Col>
-          <span>Genre: </span>
-          <span>{movie.Genre.Name}</span>
-        </Col>
-      </Row>
-
-      <Row className="row-title">
-        {/* Display the movie's director */}
-        <Col>
-          <span>Director: </span>
-          <span>{movie.Director.Name}</span>
-        </Col>
-      </Row>
-
-      <Row>
-      <Col xs={3} >
-        <Link to="/movies" className="btn btn-primary">
-          Back
-        </Link>
-      </Col>
-      <Col xs={3}>
-        {user && (
-          <Button className="favorite-button" variant="primary" onClick={handleAddFavorite}>
-            Favorite &#x2665;
-          </Button>
-        )}
-      </Col>
-    </Row>
     </div>
   );
 };
