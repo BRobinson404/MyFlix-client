9d3f689368c738ae6f2329ac7d1e5103c18c664f
BRobinson404 <brobinso123@gmail.com>
2023-10-16 18:44:04 -0500

    Update signup-view.jsx

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from "react-router-dom";
-import { Form, Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Form, Row, Col, Button, Container } from 'react-bootstrap';
 
 export const SignupView = () => {
   const [username, setUsername] = useState('');
@@ -35,70 +35,63 @@ export const SignupView = () => {
   };
 
   return (
-    <Form className='signup-form' onSubmit={handleSubmit}>
-      <Row>
-        <Col xs={6}>
-          <Form.Group controlId='formUsername'>
-            <Form.Label>Username:</Form.Label>
-            <Form.Control
-              type='text'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-              minLength='3'
-            />
-          </Form.Group>
+    <Container className="mt-5">
+      <Row className="justify-content-center">
+        <Col xs={10} sm={8} md={6} lg={4}>
+          <Form className="p-4 shadow" onSubmit={handleSubmit}>
+            <h2 className="mb-4 text-center">Sign Up</h2>
+
+            <Form.Group controlId="formUsername">
+              <Form.Label>Username:</Form.Label>
+              <Form.Control
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                minLength="3"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password:</Form.Label>
+              <Form.Control
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formEmail">
+              <Form.Label>Email:</Form.Label>
+              <Form.Control
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBirthday">
+              <Form.Label>Birthday:</Form.Label>
+              <Form.Control
+                type="date"
+                value={birthday}
+                onChange={(e) => setBirthday(e.target.value)}
+                required
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" className="w-100 mt-3">
+              Submit
+            </Button>
+
+            <p className="mt-3 text-center">
+              Already have an account? <Link to="/login">Login</Link>
+            </p>
+          </Form>
         </Col>
       </Row>
-      <Row>
-        <Col xs={6}>
-          <Form.Group controlId='formPassword'>
-            <Form.Label>Password:</Form.Label>
-            <Form.Control
-              type='password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={6}>
-          <Form.Group controlId='formEmail'>
-            <Form.Label>Email:</Form.Label>
-            <Form.Control
-              type='email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={6}>
-          <Form.Group controlId='formBirthday'>
-            <Form.Label>Birthday:</Form.Label>
-            <Form.Control
-              type='date'
-              value={birthday}
-              onChange={(e) => setBirthday(e.target.value)}
-              required
-            />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col  xs={3}>
-          <Button type='submit'>Submit</Button>
-        </Col>
-        <Col xs={3}>
-          <Link to={`/login`}>
-            I already have an account.
-          </Link>
-        </Col>
-      </Row>
-    </Form>
+    </Container>
   );
 };
