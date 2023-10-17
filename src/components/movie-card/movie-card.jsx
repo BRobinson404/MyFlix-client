d43f02c9ded4b2f39e612b275a0e292702afe9d4
BRobinson404 <brobinso123@gmail.com>
2023-10-17 02:04:46 -0500

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
