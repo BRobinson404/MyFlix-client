aadc3ddedf9dc6e6e58e857f9a97aae5738ce61c
BRobinson404 <brobinso123@gmail.com>
2023-10-18 02:09:33 -0500

    Update navigation-bar.jsx

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -20,7 +20,6 @@ export const NavigationBar = ({ user, onLoggedOut, onGenreFilter }) => {
 
   return (
     <>
-    {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand) => (
     <Navbar
         id="custom-navigation-bar"
         className="navigation-bar"
@@ -29,74 +28,72 @@ export const NavigationBar = ({ user, onLoggedOut, onGenreFilter }) => {
         expand="xl"
       >
         <Container fluid>
-  <Navbar.Brand as={Link} to="/" id="custom-navbar-brand">
-    MyFlix
-  </Navbar.Brand>
-  <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-
- <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-        <Offcanvas.Header closeButton>
-        <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>Menu</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <Nav className="me-auto">
-            {!user && (
-              <>
-                <Nav.Link as={Link} to="/login">
-                  Home
-                </Nav.Link>
-                <Nav.Link as={Link} to="/login" id="custom-nav-link-login">
-                   Login
-                </Nav.Link>
-                 <Nav.Link as={Link} to="/signup" id="custom-nav-link-signup">
-                   Sign Up
-                </Nav.Link>
-              </>
-            )}
-            {user && (
-              <>
-                <Nav.Link as={Link} to="/" className="navbar-link" id="custom-nav-link-home">
-                   Home
-                </Nav.Link>
-                <Nav.Link as={Link} to={`/users/${user.Username}`}>
-                  Profile
-                </Nav.Link>
-                <Nav.Link as={Link} to="/login" onClick={handleLogout} id="custom-nav-link-logout">
-                  Logout
-                </Nav.Link>
-              </>
-            )}
-          </Nav>
-          {user && (
-            <Nav id="custom-dropdown-menu">
-              <Dropdown>
-                <Dropdown.Toggle as={Button} variant="secondary" id="dropdown-genre">
-                  Filter Genres
-                </Dropdown.Toggle>
-                <Dropdown.Menu id="custom-dropdown-menu-items">
-                  <Dropdown.Item onClick={() => handleGenreFilter('')}>All Genres</Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleGenreFilter('Crime')}>Crime</Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleGenreFilter('Science Fiction')}>Science Fiction</Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleGenreFilter('Comedy-drama')}>Comedy-drama</Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleGenreFilter('Superhero')}>Superhero</Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleGenreFilter('Action')}>Action</Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleGenreFilter('Drama')}>Drama</Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleGenreFilter('Horror')}>Horror</Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleGenreFilter('Thriller')}>Thriller</Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleGenreFilter('Romance')}>Romance</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </Nav>
-          )}
-        </Offcanvas.Body>
-      </Navbar.Offcanvas>
-      </Container>
+          <Navbar.Brand as={Link} to="/" id="custom-navbar-brand">
+            MyFlix
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel">Menu</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="me-auto">
+                {!user && (
+                  <>
+                    <Nav.Link as={Link} to="/login">
+                      Home
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/login" id="custom-nav-link-login">
+                      Login
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/signup" id="custom-nav-link-signup">
+                      Sign Up
+                    </Nav.Link>
+                  </>
+                )}
+                {user && (
+                  <>
+                    <Nav.Link as={Link} to="/" className="navbar-link" id="custom-nav-link-home">
+                      Home
+                    </Nav.Link>
+                    <Nav.Link as={Link} to={`/users/${user.Username}`}>
+                      Profile
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/login" onClick={handleLogout} id="custom-nav-link-logout">
+                      Logout
+                    </Nav.Link>
+                  </>
+                )}
+              </Nav>
+              {user && (
+                <Nav id="custom-dropdown-menu">
+                  <Dropdown>
+                    <Dropdown.Toggle as={Button} variant="secondary" id="dropdown-genre">
+                      Filter Genres
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu id="custom-dropdown-menu-items">
+                      <Dropdown.Item onClick={() => handleGenreFilter('')}>All Genres</Dropdown.Item>
+                      <Dropdown.Item onClick={() => handleGenreFilter('Crime')}>Crime</Dropdown.Item>
+                      <Dropdown.Item onClick={() => handleGenreFilter('Science Fiction')}>Science Fiction</Dropdown.Item>
+                      <Dropdown.Item onClick={() => handleGenreFilter('Comedy-drama')}>Comedy-drama</Dropdown.Item>
+                      <Dropdown.Item onClick={() => handleGenreFilter('Superhero')}>Superhero</Dropdown.Item>
+                      <Dropdown.Item onClick={() => handleGenreFilter('Action')}>Action</Dropdown.Item>
+                      <Dropdown.Item onClick={() => handleGenreFilter('Drama')}>Drama</Dropdown.Item>
+                      <Dropdown.Item onClick={() => handleGenreFilter('Horror')}>Horror</Dropdown.Item>
+                      <Dropdown.Item onClick={() => handleGenreFilter('Thriller')}>Thriller</Dropdown.Item>
+                      <Dropdown.Item onClick={() => handleGenreFilter('Romance')}>Romance</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Nav>
+              )}
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
       </Navbar>
-    ))}
     </>
   );
 };
