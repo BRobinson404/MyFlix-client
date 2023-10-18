a35e75121b2ee82c70eaa8fd4aeebd6f84e99d3b
BRobinson404 <brobinso123@gmail.com>
2023-10-17 23:48:28 -0500

    Update navigation-bar.jsx

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Navbar, Nav, Button, Dropdown } from 'react-bootstrap';
+import { Navbar, Nav, Button, Dropdown, Offcanvas } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import "./navigation-bar.scss"
@@ -18,6 +18,7 @@ export const NavigationBar = ({ user, onLoggedOut, onGenreFilter }) => {
   };
 
   return (
+    <>
     <Navbar id="custom-navigation-bar" className="navigation-bar" sticky="top" data-bs-theme="dark" expand="xl">
   <Navbar.Brand as={Link} to="/" id="custom-navbar-brand">
     MyFlix
@@ -75,6 +76,62 @@ export const NavigationBar = ({ user, onLoggedOut, onGenreFilter }) => {
     )}
   </Navbar.Collapse>
 </Navbar>
-
+<Offcanvas show={showOffCanvas} onHide={() => setShowOffCanvas(false)}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Nav className="me-auto">
+            {!user && (
+              <>
+                <Nav.Link as={Link} to="/login">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/login" id="custom-nav-link-login">
+                   Login
+                </Nav.Link>
+                 <Nav.Link as={Link} to="/signup" id="custom-nav-link-signup">
+                   Sign Up
+                </Nav.Link>
+              </>
+            )}
+            {user && (
+              <>
+                <Nav.Link as={Link} to="/" className="navbar-link" id="custom-nav-link-home">
+                   Home
+                </Nav.Link>
+                <Nav.Link as={Link} to={`/users/${user.Username}`}>
+                  Profile
+                </Nav.Link>
+                <Nav.Link as={Link} to="/login" onClick={handleLogout} id="custom-nav-link-logout">
+                  Logout
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+          {user && (
+            <Nav id="custom-dropdown-menu">
+              <Dropdown show={showGenreFilter} onToggle={(isOpen) => setShowGenreFilter(isOpen)}>
+                <Dropdown.Toggle as={Button} variant="secondary" id="dropdown-genre">
+                  Filter Genres
+                </Dropdown.Toggle>
+                <Dropdown.Menu id="custom-dropdown-menu-items">
+                  <Dropdown.Item onClick={() => handleGenreFilter('')}>All Genres</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleGenreFilter('Crime')}>Crime</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleGenreFilter('Science Fiction')}>Science Fiction</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleGenreFilter('Comedy-drama')}>Comedy-drama</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleGenreFilter('Superhero')}>Superhero</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleGenreFilter('Action')}>Action</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleGenreFilter('Drama')}>Drama</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleGenreFilter('Horror')}>Horror</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleGenreFilter('Thriller')}>Thriller</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleGenreFilter('Romance')}>Romance</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Nav>
+          )}
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
   );
 };
