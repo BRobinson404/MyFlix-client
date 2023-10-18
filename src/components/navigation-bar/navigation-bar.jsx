4963d8305ff1b0c1316f3cfd5ab24c420559c674
BRobinson404 <brobinso123@gmail.com>
2023-10-18 01:42:24 -0500

    Update navigation-bar.jsx

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -42,59 +42,6 @@ export const NavigationBar = ({ user, onLoggedOut, onGenreFilter }) => {
     MyFlix
   </Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-  {isNavbarCollapsed ? null : (
-    <Nav className="me-auto">
-      {!user && (
-        <>
-          <Nav.Link as={Link} to="/login" className="navbar-link" id="custom-nav-link-home">
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to="/login" id="custom-nav-link-login">
-            Login
-          </Nav.Link>
-          <Nav.Link as={Link} to="/signup" id="custom-nav-link-signup">
-            Sign Up
-          </Nav.Link>
-        </>
-      )}
-      {user && (
-        <>
-          <Nav.Link as={Link} to="/" className="navbar-link" id="custom-nav-link-home">
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to={`/users/${user.Username}`} id="custom-nav-link-profile">
-            Profile
-          </Nav.Link>
-          <Nav.Link as={Link} to="/login" onClick={handleLogout} id="custom-nav-link-logout">
-            Logout
-          </Nav.Link>
-        </>
-      )}
-    </Nav>
-  )}
-    {user && (
-      <Nav id="custom-dropdown-menu">
-        <Dropdown show={showGenreFilter} onToggle={(isOpen) => setShowGenreFilter(isOpen)}>
-          <Dropdown.Toggle as={Button} variant="secondary" id="dropdown-genre">
-            Filter Genres
-          </Dropdown.Toggle>
-          <Dropdown.Menu id="custom-dropdown-menu-items">
-                <Dropdown.Item onClick={() => handleGenreFilter('')}>All Genres</Dropdown.Item>
-                <Dropdown.Item onClick={() => handleGenreFilter('Crime')}>Crime</Dropdown.Item>
-                <Dropdown.Item onClick={() => handleGenreFilter('Science Fiction')}>Science Fiction</Dropdown.Item>
-                <Dropdown.Item onClick={() => handleGenreFilter('Comedy-drama')}>Comedy-drama</Dropdown.Item>
-                <Dropdown.Item onClick={() => handleGenreFilter('Superhero')}>Superhero</Dropdown.Item>
-                <Dropdown.Item onClick={() => handleGenreFilter('Action')}>Action</Dropdown.Item>
-                <Dropdown.Item onClick={() => handleGenreFilter('Drama')}>Drama</Dropdown.Item>
-                <Dropdown.Item onClick={() => handleGenreFilter('Horror')}>Horror</Dropdown.Item>
-                <Dropdown.Item onClick={() => handleGenreFilter('Thriller')}>Thriller</Dropdown.Item>
-                <Dropdown.Item onClick={() => handleGenreFilter('Romance')}>Romance</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Nav>
-    )}
-  </Navbar.Collapse>
 </Navbar>
  <Offcanvas show={showOffCanvas} onHide={() => setShowOffCanvas(false)} placement="end">
         <Offcanvas.Header closeButton>
